04eb71dd0660881b2afe25fe0fd1dce36a588477
robin200912 <34182390+robin200912@users.noreply.github.com>
2018-05-08 21:11:44 +0800

    Update contact-test.py

--- 05.数据分析/pandas/contact-test.py
+++ 05.数据分析/pandas/contact-test.py
@@ -1,1 +1,21 @@
-import pandas as pda1 = pd.DataFrame({    'a': [1, 2, 3],    'b': [4, 5, 6],    'c': [7, 8, 9]})a2 = pd.DataFrame({    'd': [10, 11]})b = pd.concat([a1, a2], axis=1)print ba3 = pd.DataFrame({    'd': [12, 13, 14]})c = b.combine_first(a3)print c
+import pandas as pd
+
+a1 = pd.DataFrame({
+    'a': [1, 2, 3],
+    'b': [4, 5, 6],
+    'c': [7, 8, 9]
+})
+
+a2 = pd.DataFrame({
+    'd': [10, 11]
+})
+
+b = pd.concat([a1, a2], axis=1)
+print b
+
+a3 = pd.DataFrame({
+    'd': [12, 13, 14]
+})
+
+c = b.combine_first(a3)
+print c 
