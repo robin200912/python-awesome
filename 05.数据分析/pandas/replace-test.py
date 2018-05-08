555c65a728ccd92c8f138d8381b6a591ae95e28a
robin200912 <34182390+robin200912@users.noreply.github.com>
2018-05-08 21:15:13 +0800

    Update replace-test.py

--- 05.数据分析/pandas/replace-test.py
+++ 05.数据分析/pandas/replace-test.py
@@ -1,1 +1,11 @@
-import pandas as pdimport numpy as npa = pd.DataFrame({    'a': np.arange(1, 4),    'b': np.arange(4, 7),    'c': np.arange(7, 10),})b = a.replace([1, 2], [11, 22])print b
+import pandas as pd
+import numpy as np
+
+a = pd.DataFrame({
+    'a': np.arange(1, 4),
+    'b': np.arange(4, 7),
+    'c': np.arange(7, 10),
+})
+
+b = a.replace([1, 2], [11, 22])
+print b 
