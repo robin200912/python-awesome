4edefeeed05bd4c0bda7cb95e88f4f329fe0ef5f
Robin201711 <2426559320@qq.com>
2018-05-08 21:00:13 +0800

    update code

--- 05.数据分析/pandas/bytes-file-test.py
+++ 05.数据分析/pandas/bytes-file-test.py
@@ -1,1 +1,1 @@
-import pandas as pda = pd.DataFrame({'column1': [1, 2, 3], 'column2': [4, 5, 6]})a.to_pickle('test')print ab = pd.read_pickle('test')print type(b)print b
+import pandas as pda = pd.DataFrame({'column1': [1, 2, 3], 'column2': [4, 5, 6]})a.to_pickle('test')print ab = pd.read_pickle('test')print type(b)print b
