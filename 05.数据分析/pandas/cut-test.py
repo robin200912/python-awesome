4976d312d9e8bf8da9d635b89e044470ee319377
robin200912 <34182390+robin200912@users.noreply.github.com>
2018-05-08 21:11:58 +0800

    Update cut-test.py

--- 05.数据分析/pandas/cut-test.py
+++ 05.数据分析/pandas/cut-test.py
@@ -1,1 +1,13 @@
-import pandas as pdimport numpy as npa = np.random.randint(1, 100, 10)print ab = pd.cut(a, [0, 20, 60, 80, 100], labels=['a', 'b', 'c', 'd'])print bprint pd.value_counts(b)c = pd.qcut(a, 5)print cprint pd.value_counts(c)
+import pandas as pd
+import numpy as np
+
+a = np.random.randint(1, 100, 10)
+print a
+
+b = pd.cut(a, [0, 20, 60, 80, 100], labels=['a', 'b', 'c', 'd'])
+print b
+print pd.value_counts(b)
+
+c = pd.qcut(a, 5)
+print c
+print pd.value_counts(c) 
