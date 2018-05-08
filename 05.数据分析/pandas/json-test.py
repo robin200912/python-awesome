09b9c877486a0c2183c2a73b70bfd7a3e36495c1
robin200912 <34182390+robin200912@users.noreply.github.com>
2018-05-08 21:13:18 +0800

    Update json-test.py

--- 05.数据分析/pandas/json-test.py
+++ 05.数据分析/pandas/json-test.py
@@ -1,1 +1,13 @@
-import jsonimport pandas as pda = json.loads('[{"a": 1}]')print a, type(a)b = json.dumps(a)print b, type(b)c = pd.DataFrame(a)print c
+import json
+import pandas as pd
+
+
+a = json.loads('[{"a": 1}]')
+print a, type(a)
+
+b = json.dumps(a)
+print b, type(b)
+
+c = pd.DataFrame(a)
+print c 
+
