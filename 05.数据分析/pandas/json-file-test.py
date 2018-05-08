bdd12cc91dd53b45522a19586105550cdee9f393
robin200912 <34182390+robin200912@users.noreply.github.com>
2018-05-08 21:13:05 +0800

    Update json-file-test.py

--- 05.数据分析/pandas/json-file-test.py
+++ 05.数据分析/pandas/json-file-test.py
@@ -1,1 +1,9 @@
-import pandas as pda = pd.DataFrame({'column1': [1, 2, 3], 'column2': [4, 5, 6]})a.to_json('test.json')b = pd.read_json('test.json')print type(b)print b
+import pandas as pd
+
+a = pd.DataFrame({'column1': [1, 2, 3], 'column2': [4, 5, 6]})
+
+a.to_json('test.json')
+
+b = pd.read_json('test.json')
+print type(b)
+print b 
