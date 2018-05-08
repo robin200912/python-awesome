c94e4b3a102cb41035cb28fb04fb275c589a7ba9
robin200912 <34182390+robin200912@users.noreply.github.com>
2018-05-08 21:15:01 +0800

    Update rename-test.py

--- 05.数据分析/pandas/rename-test.py
+++ 05.数据分析/pandas/rename-test.py
@@ -1,1 +1,11 @@
-import pandas as pdimport numpy as npa = pd.DataFrame({    'a': np.arange(1, 4),    'b': np.arange(4, 7),    'c': np.arange(7, 10),})a.rename(index={0: '00'}, columns={'a': 'aa'}, inplace=True)print a
+import pandas as pd
+import numpy as np
+
+a = pd.DataFrame({
+    'a': np.arange(1, 4),
+    'b': np.arange(4, 7),
+    'c': np.arange(7, 10),
+})
+
+a.rename(index={0: '00'}, columns={'a': 'aa'}, inplace=True)
+print a 
