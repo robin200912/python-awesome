069e0f77a027b030c18ffe32765643a6f840206f
robin200912 <34182390+robin200912@users.noreply.github.com>
2018-05-08 21:15:26 +0800

    Update sample-test.py

--- 05.数据分析/pandas/sample-test.py
+++ 05.数据分析/pandas/sample-test.py
@@ -1,1 +1,10 @@
-import pandas as pdimport numpy as npa = pd.DataFrame(np.arange(20).reshape(5, 4))print ab = np.random.permutation(5)print bc = a.take(b)[:3]print c
+import pandas as pd
+import numpy as np
+
+a = pd.DataFrame(np.arange(20).reshape(5, 4))
+print a
+b = np.random.permutation(5)
+print b
+
+c = a.take(b)[:3]
+print c 
