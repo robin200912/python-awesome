1cc0db4c74fb1550520ed4c0eaecc83432950367
robin200912 <34182390+robin200912@users.noreply.github.com>
2018-05-08 21:15:37 +0800

    Update serias-test.py

--- 05.数据分析/pandas/serias-test.py
+++ 05.数据分析/pandas/serias-test.py
@@ -1,1 +1,16 @@
-import pandas as pda = pd.Series(['a', 'b', 'c'])b = pd.Series({'a': 1, 'b': 2, 'c': 3})print aprint ba.index = ['a1', 'a2', 'a3']print ab['a2'] = 22b['xxx'] = 55b.a3 = 33b.index.name = 'test'b.name = 'test-name'print b
+import pandas as pd
+
+a = pd.Series(['a', 'b', 'c'])
+b = pd.Series({'a': 1, 'b': 2, 'c': 3})
+print a
+print b
+
+a.index = ['a1', 'a2', 'a3']
+print a
+
+b['a2'] = 22
+b['xxx'] = 55
+b.a3 = 33
+b.index.name = 'test'
+b.name = 'test-name'
+print b 
