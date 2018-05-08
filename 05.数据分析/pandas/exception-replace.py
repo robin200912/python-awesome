c78b1fcefe3b64eabe7da5c6af3c90cf292f6ba9
robin200912 <34182390+robin200912@users.noreply.github.com>
2018-05-08 21:12:53 +0800

    Update exception-replace.py

--- 05.数据分析/pandas/exception-replace.py
+++ 05.数据分析/pandas/exception-replace.py
@@ -1,1 +1,9 @@
-import pandas as pdimport numpy as npa = pd.DataFrame(np.random.randn(1000, 5))print a.describe()a[np.abs(a) > 3] = np.sign(a) * 3print a.describe()
+import pandas as pd
+import numpy as np
+
+
+a = pd.DataFrame(np.random.randn(1000, 5))
+print a.describe()
+
+a[np.abs(a) > 3] = np.sign(a) * 3
+print a.describe() 
