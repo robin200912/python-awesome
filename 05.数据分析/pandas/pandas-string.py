3043bf4a142acf9a740e254d46500c29407ff1db
robin200912 <34182390+robin200912@users.noreply.github.com>
2018-05-08 21:14:16 +0800

    Update pandas-string.py

--- 05.数据分析/pandas/pandas-string.py
+++ 05.数据分析/pandas/pandas-string.py
@@ -1,1 +1,15 @@
-import pandas as pdimport rea = pd.Series({'robin': 'robin201107@126.com', 'zhanfei': 'zhanfei2018@gmail.com'})print a.strb = a.str.contains('gmail')print type(b)reg = re.compile('\d+')c = a.str.findall(reg)d = a.str.replace(reg, '')e = a.str.split('@').map(lambda x: ','.join(x))r = pd.concat([a, b, c, d, e], axis=1)print r
+import pandas as pd
+import re
+
+a = pd.Series({'robin': 'robin201107@126.com', 'zhanfei': 'zhanfei2018@gmail.com'})
+print a.str
+b = a.str.contains('gmail')
+print type(b)
+
+reg = re.compile('\d+')
+c = a.str.findall(reg)
+d = a.str.replace(reg, '')
+e = a.str.split('@').map(lambda x: ','.join(x))
+
+r = pd.concat([a, b, c, d, e], axis=1)
+print r 
