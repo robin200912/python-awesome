287d780a4f39294fff5174fdcfd6ee709936a82c
robin200912 <34182390+robin200912@users.noreply.github.com>
2018-05-08 21:14:28 +0800

    Update pivot-test.py

--- 05.数据分析/pandas/pivot-test.py
+++ 05.数据分析/pandas/pivot-test.py
@@ -1,1 +1,12 @@
-import pandas as pda = pd.DataFrame({    'date': ['5-1', '5-1', '5-2', '5-2'],    'item': ['price', 'sum', 'price', 'sum'],    'value': [4.2, 44, 5.5, 66]})print ab = a.pivot('date', 'item', 'value')print b
+import pandas as pd
+
+a = pd.DataFrame({
+    'date': ['5-1', '5-1', '5-2', '5-2'],
+    'item': ['price', 'sum', 'price', 'sum'],
+    'value': [4.2, 44, 5.5, 66]
+})
+print a
+
+b = a.pivot('date', 'item', 'value')
+print b 
+
