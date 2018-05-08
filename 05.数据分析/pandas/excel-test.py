40c3d4cb129f8001802edbf4dd3e131ee2bc6980
robin200912 <34182390+robin200912@users.noreply.github.com>
2018-05-08 21:12:41 +0800

    Update excel-test.py

--- 05.数据分析/pandas/excel-test.py
+++ 05.数据分析/pandas/excel-test.py
@@ -1,1 +1,11 @@
-import pandas as pda = pd.DataFrame({'column1': [1, 2, 3], 'column2': [4, 5, 6]})a.to_excel('test.xls')b = pd.ExcelFile('test.xls')c = b.parse()print type(c)print c
+import pandas as pd
+
+a = pd.DataFrame({'column1': [1, 2, 3], 'column2': [4, 5, 6]})
+
+a.to_excel('test.xls')
+
+b = pd.ExcelFile('test.xls')
+c = b.parse()
+print type(c)
+print c 
+
