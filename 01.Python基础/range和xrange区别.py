f0a91825c9a94745670b417b91b2b6bf72d0d135
Robin201711 <2426559320@qq.com>
2018-05-09 16:35:36 +0800

    update code

--- 01.Python基础/range和xrange区别.py
+++ 01.Python基础/range和xrange区别.py
@@ -8,7 +8,6 @@ end = time.time()
 print end - start  # 3.60325598717
 
 # xrange则不会直接生成一个list，而是每次调用返回其中的一个值
-# 所以xrange做循环的性能比range好，尤其是返回很大的时候。尽量用xrange吧，除非你是要返回一个列表
 start = time.time()
 b = xrange(100000000)
 print type(b)
