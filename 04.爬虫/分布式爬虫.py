6b81504486f3f58238dc3c23cc3b9d4e7cc814db
Robin201711 <2426559320@qq.com>
2018-05-10 10:38:04 +0800

    update code

---
+++ 04.爬虫/分布式爬虫.py
@@ -0,0 +1,23 @@
+# pip install scrapy-redis
+
+# -*- coding: utf-8 -*-
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy_redis.spiders import RedisCrawlSpider
+
+
+class CrawlRedisSpiderSpider(RedisCrawlSpider):
+    name = 'baidu'
+    allowed_domains = ['www.baidu.com']
+    redis_key = 'baidu:start_urls'
+
+    rules = (
+        Rule(LinkExtractor(allow=r''), follow=True),
+        Rule(LinkExtractor(allow=r''), callback='parse_item', follow=False),
+    )
+
+    def parse_item(self, response):
+        data = {
+            'title': response.xpath('//title/text()').extract_first(),
+        }
+        return data
