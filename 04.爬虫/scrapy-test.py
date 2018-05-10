60f8a5413ce05efa747f61edb1d4ad53893117c4
Robin201711 <2426559320@qq.com>
2018-05-10 08:26:53 +0800

    update code

---
+++ 04.爬虫/scrapy-test.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class BaiduSpider(scrapy.Spider):
+    name = 'baidu'
+    allowed_domains = ['baidu.com']
+    start_urls = ['http://www.baidu.com/']
+
+    def parse(self, response):
+        print response.xpath('//a/text()').extract_first()
+
+        return {
+            'url': response.url
+        }
